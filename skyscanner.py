36148d2a3e0669b91479b7561456b3a645b773fb
lydl28 <lydia.kalkbrenner@hotmail.de>
2016-10-16 01:29:56 +0200

    back to the roots

--- skyscanner.py
+++ skyscanner.py
@@ -3,14 +3,11 @@
 import requests
 import json
 
-API_KEY = 'ja134927235879517824145636024746'
+API_KEY = open('token').read()
 
 #http://partners.api.skyscanner.net/apiservices/browseroutes/v1.0/{market}/{currency}/{locale}/{originPlace}/{destinationPlace}/{outboundPartialDate}/{inboundPartialDate}?apiKey={apiKey}
 autosuggest = "http://partners.api.skyscanner.net/apiservices/autosuggest/v1.0/GB/USD/EN/?query={}&apiKey={}"
-#browse_quotes = "http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/GB/USD/en-GB/{}/{}/{}/2017-02-15?apiKey={}"
-pricing = "http://partners.api.skyscanner.net/apiservices/pricing/v1.0"
-header = {"accept" : "application/json", "content-type" : "application/x-www-form-urlencoded"}
-
+browse_quotes = "http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/GB/USD/en-GB/{}/{}/{}/2017-02-15?apiKey={}"
 
 # Formats the date to yyyy-mm-dd
 def dateFormat(day, month, year):
@@ -27,16 +24,12 @@ def get_price(origin_query, destination_query, date_query):
 	origin_id = origin["Places"][0]['CityId']
 	destination_id = destination["Places"][0]['CityId']
 
-	payload = {'apiKey' : API_KEY, 'country' : 'GB', 'currency': 'USD', 'locale' : 'EN', 'originplace' : origin_id, 'destinationplace' : destination_id, 'outbounddate' : date_query}
 	# get result
-	#import pdb; pdb.set_trace()
-	result = requests.post(pricing, json = payload, headers = header)
-	return result.json
-
-	#result["Quotes"][0]["MinPrice"]
+	result = json.loads(requests.get(browse_quotes.format(origin_id, destination_id, date_query, API_KEY)).text)
+	return result["Quotes"][0]["MinPrice"]
 
 
-print(get_price("Toronto","Berlin", "2017-02-15"))
+#print(get_price("Toronto","Berlin", "2017-02-15"))
 
 # location_query = "Barcelona"
 # locations = json.loads(requests.get(autosuggest.format(location_query, API_KEY)).text)
